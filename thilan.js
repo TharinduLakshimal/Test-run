199c7f68e6f512f0e12a1c49e2c715b6ee62b6b1
Your Name <you@example.com>
2024-06-21 14:08:27 +0530

    create testmodel

--- thilan.js
+++ thilan.js
@@ -1,1 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+//schema
+const testmodel = new mongoose.Schema(
+  {
+    test1: {
+      type: String,
+      required: [true, "test name required"],
+    },
+    test2: {
+      type: String,
+    },
+    test3: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const testmod = mongoose.model("canteen", testmodel);
+
+module.exports = testmod;
